e85fb6a447e27fac7e8b5874305d0d98ee36ff23
Tsirkin Pavel <master878@yandex.ru>
2024-01-26 16:00:44 +0300

    решение задачи1

--- Task1_1/Program.cs
+++ Task1_1/Program.cs
@@ -1,6 +1,21 @@
-﻿int N = 5, i = 1;
-while (i <= N)
-{
-    Console.WriteLine(i);
-    i++;
-}
+﻿//int N = 5, i = 1;
+//while (i <= N)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+// ЗАДАНИЕ1 нахождение большего из 2 чисел
+//int firstNumber = 5;
+//int secondNumber = 6;
+//if (firstNumber > secondNumber)
+//{
+    //Console.WriteLine($"первое число больше");
+//}
+//else if (firstNumber < secondNumber)
+//{
+    //Console.WriteLine($"второе число больше");
+//}
+//else
+//{
+    //Console.WriteLine($"числа равны");
+//}
