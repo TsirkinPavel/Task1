590e905e672746bb590b81c2e24565acc165b09f
Tsirkin Pavel <master878@yandex.ru>
2024-01-26 19:53:13 +0300

    решение задач1-4 первого дз

--- Task1_1/Program.cs
+++ Task1_1/Program.cs
@@ -9,13 +9,46 @@
 //int secondNumber = 6;
 //if (firstNumber > secondNumber)
 //{
-    //Console.WriteLine($"первое число больше");
+//Console.WriteLine($"первое число больше");
 //}
 //else if (firstNumber < secondNumber)
 //{
-    //Console.WriteLine($"второе число больше");
+//Console.WriteLine($"второе число больше");
 //}
 //else
 //{
-    //Console.WriteLine($"числа равны");
+//Console.WriteLine($"числа равны");
 //}
+
+//ЗАДАНИЕ2 максимальное из 3 чисел
+//int a = 5; 
+//int b = 6;
+//int c = 7;
+//int result = a;
+//if (b > result)
+//{
+//result = b;
+//}
+//if (c > result)
+//{
+//result = c;
+//}
+//System.Console.WriteLine($"{result}");
+
+//ЗАДАНИЕ3 Проверка на четность
+//int number = 11;
+//if (number % 2 == 0)
+//{
+    //Console.WriteLine($"четное");
+//}
+//else
+//{
+    //Console.WriteLine($"Число {number} - нечетное");
+//}
+
+//ЗАДАНИЕ 4 Вывести четные числа
+int number = 8;
+for (int i = 2; i <= number; i+=2)
+{
+   Console.Write($"{i} "); 
+}
