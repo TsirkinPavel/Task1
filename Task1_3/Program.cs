184085ef0de6c92905b6d49f66a506f6dbdfba18
Tsirkin Pavel <master878@yandex.ru>
2024-02-05 02:30:45 +0300

    Знакомство с языками программирования семинар 4

--- Task1_3/Program.cs
+++ Task1_3/Program.cs
@@ -34,7 +34,7 @@ System.Console.WriteLine(count);
 //Задание 3. Задайте массив из вещественных чисел с ненулевой дробной частью.
 //Найдите разницу между максимальным и минимальным элементов массива.
 
-double[] numbers = { 0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01 };
+/*double[] numbers = { 0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01 };
 double FindMin(double[] numbers)
 {
     double Min = numbers[0];
@@ -65,3 +65,81 @@ double FindMax(double[] numbers)
 double Max = FindMax(numbers);
 Console.WriteLine(Max);
 Console.WriteLine(Max - Min);
+*/
+
+//ЗАПОЛНЕНИЕ МАССИВА СЛУЧАЙНЫМИ ЧИСЛАМИ
+/*Random rnd  = new Random();
+int size = 6;
+int[] arr_int = new int[size];
+int i = 0;
+while(i < size)
+{
+	arr_int[i] = rnd.Next(1, 1000);
+	i++;
+}
+
+i = 0;
+while(i < size)
+{
+	Console.Write($"{arr_int[i]} ");
+	i++;
+}
+*/
+//Семинар 4 
+//Задание 1. Задайте одномерный массив, заполненный случайными
+//числами. Определите количество простых чисел в этом массиве.
+Random rnd = new Random();
+int size = 6;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    arr_int[i] = rnd.Next(1, 100);
+    i++;
+}
+
+//определяем простое число или нет
+bool IsPrime(int number)
+{
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+    //Задание 2. Задайте массив из N случайных целых чисел (N вводится с
+    //клавиатуры). Найдите количество чисел, которые оканчиваются на 1 и
+    //делятся нацело на 7.
+    /*
+    System.Console.WriteLine("Введите количество элементов массива:");
+    int N = int.Parse(Console.ReadLine());
+    Random rnd = new Random();
+    int size = N;
+    int[] arr_int = new int[size];
+    int i = 0;
+    while (i < size)
+    {
+        arr_int[i] = rnd.Next(1, 100);
+        i++;
+    }
+    */
+    /*Задание 3. Заполните массив на N (вводится с консоли, не более 8)
+    случайных целых чисел от 0 до 9.
+    Сформируйте целое число, которое будет состоять из цифр из
+    массива. Старший разряд числа находится на 0-м индексе,
+    младший – на последнем.*/
+    /*
+    int []array = new int [8]
+    for (int i = 0; i < 8; i++)
+    {
+        System.Console.WriteLine("Введите {i+1}-й элемент массива, любое число от 0 до 9:");
+        int value = int.Parse(Console.ReadLine());
+        int array[i] = value        
+    }
+string StrNum = Num.ToString(array);
+for (int i = 0; i < StrNum.Length); i++)
+{
+    System.Console.Write($"{StrNum[i]}");
+}
+*/
