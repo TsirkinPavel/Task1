ab4adfbd32e470634b39ac4ac73b016bf37c7fa8
Tsirkin Pavel <master878@yandex.ru>
2024-02-04 21:27:16 +0300

    Домашнее задание№3. Решение 3 задачи. Знакомство с языками программирования (семинары)

---
+++ Task1_3/Program.cs
@@ -0,0 +1,50 @@
+﻿// ДЗ №3 Знакомство с языками программирования (семинары)
+
+// Задание 1. Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,90].
+/*int[] numbers = { 1, 5, 10, 20, 30, 40, 99, 4, 90, 3 };
+int minRange = 10;
+int maxRange = 90;
+int count = 0;
+foreach (int element in numbers)
+{
+    if (element >= minRange && element <= maxRange)
+    {
+        count++;
+    }
+}
+//return count;
+System.Console.WriteLine(count);
+*/
+// Задание 2. Задайте массив на 10 целых чисел.
+//Напишите программу, которая определяет количество чётных чисел в массиве.
+/*int[] numbers = { 2, 5, 2, 3, 6, 3, 7, 8, 2, 4 };
+
+int count = 0;
+foreach (int element in numbers)
+{
+    if (element % 2 ==0)
+    {
+        count++;
+    }
+}
+//return count;
+System.Console.WriteLine(count);
+*/
+//Задание 3. Задайте массив из вещественных чисел с ненулевой дробной частью.
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] numbers = { 0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01 };
+double FindMin(double[] numbers)
+{double Min = numbers[0];
+foreach (double e in numbers)
+{
+    if (e < Min)
+        {
+        Min = e;
+    }
+}
+return Min;
+}
+double Min = FindMin(double[] numbers);
+System.Console.WriteLine(Min);
