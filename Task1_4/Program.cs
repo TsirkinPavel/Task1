601b2efc3d416f97961ed684579df30cc527da20
Tsirkin Pavel <master878@yandex.ru>
2024-02-07 22:18:57 +0300

    Семинар №5 начало3

--- Task1_4/Program.cs
+++ Task1_4/Program.cs
@@ -74,7 +74,7 @@ string GetLettersFromStr(string s)
     string letters = "";
     foreach (char e in s)
     {
-        if (char.IsAskiiLetter(e) == true)
+        if (char.IsAsciiLetter(e) == true)
         {
             letters = letters + e;
         }        
