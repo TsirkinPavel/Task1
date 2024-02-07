504ef3d8247b6295c1c63d21b47ffc079ab6bc6e
Tsirkin Pavel <master878@yandex.ru>
2024-02-07 09:55:19 +0300

    Знакомство с языками программирования ДЗ №4

---
+++ Task1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Знакомство с языками программирования (семинары)
+//Урок 4. Функции Домашнее задание
+int[,] matrix = new int[2, 4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Random.rnd = new Random();
+        matrix[i, j] = rnd.Next(1, 11);
+    }
+}
+System.Console.WriteLine(matrix);
